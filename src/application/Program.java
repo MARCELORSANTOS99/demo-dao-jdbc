8349a632a9cac3f9f78ff2edc7603a0fe4c43f99
Marcelo Rodrigues dos Santos <marcelo.r.santos99@hotmail.com>
2020-05-16 12:08:39 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department obj = new Department(1,"books");
+		Seller sl = new Seller(1,"Marcelo Santos", "marcelo@hotmail.com", new Date(), 8500.0, obj);
+		
+		System.out.println(obj);
+		System.out.println(sl);
+		
+		sl.setBaseSalary(10500.0);
+		System.out.println(sl);
+		
 	}
 
 }
