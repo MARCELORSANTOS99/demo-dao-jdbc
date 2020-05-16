153278d65963a0ff50b835e3fd6b326f705083d1
Marcelo Rodrigues dos Santos <marcelo.r.santos99@hotmail.com>
2020-05-16 12:38:53 -0300

    Find by ID implementacion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"books");
-		Seller sl = new Seller(1,"Marcelo Santos", "marcelo@hotmail.com", new Date(), 8500.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
 		
-		SellerDao sellerDado = DaoFactory.createSellerDao();
-		
-		System.out.println(obj);
-		System.out.println(sl);
-		
-		sl.setBaseSalary(10500.0);
-		System.out.println(sl);
+		System.out.println(seller);
 		
 	}
 
